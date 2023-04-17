66c480a17e470498cf4fa6fb2cb4ddb64dafb74c
Striver <1312395217@qq.com>
2023-04-17 09:01:56 -0700

    add mfe code for login/logout features

---
+++ web-project/mfe-customer/src/views/IndexView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>欢迎呀</div>
+  <div>
+    <el-button @click="logout()" class="danger" plain>退出登录</el-button>
+  </div>
+</template>
+
+<script setup>
+import { ElMessage } from 'element-plus'
+import router from '../router'
+import { get } from '../net'
+
+const logout = () => {
+  get('/api/auth/logout', (message) => {
+    ElMessage.success(message)
+    router.push('/')
+  })
+}
+</script>
+
+<style scoped></style>
---
+++ web-project/mfe-customer/src/views/WelcomeView.vue
@@ -0,0 +1,27 @@
+<template>
+  <div style="width: 100vw; height: 100vh; overflow: hidden; display: flex">
+    <div style="flex: 1">
+      <el-image style="width: 100%; height: 100%" fit="cover" src="src\assets\images\bg.jpeg" />
+    </div>
+    <div style="width: 400px; background-color: white; text-align: center">
+      <router-view />
+    </div>
+  </div>
+
+  <div class="welcome-title">
+    <div style="font-size: 30px; font-weight: bold">欢迎到来！</div>
+    <div style="margin-top: 10px">全栈学习！</div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+.welcome-title {
+  position: absolute;
+  top: 30px;
+  left: 30px;
+  color: white;
+  text-shadow: 0 0 10px black;
+}
+</style>
